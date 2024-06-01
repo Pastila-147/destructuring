c4217dafc144464ea18874ae800235e6cf94273b
Evgeniya Emelina <evg.emelina@gmail.com>
2024-06-01 21:05:06 +0200

    решение задачи Destructuring

---
+++ src/js/__tests__/getAttackType.js
@@ -0,0 +1,39 @@
+import getAttackType from '../functions';
+
+test('test getAttackType function', () => {
+  const special = getAttackType({
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  });
+  expect(special).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+});
